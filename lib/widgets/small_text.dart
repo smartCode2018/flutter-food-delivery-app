92c1b94e39cb25d3e69f5cfb5e429f06a17675c5
Justice Kelechi <smartcode.kc@gmail.com>
2022-12-23 17:31:49 -0300

    first commit

---
+++ lib/widgets/small_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+class SmallText extends StatelessWidget {
+  Color? color;
+  final String text;
+  double size;
+  double height;
+  SmallText(
+      {Key? key,
+      this.color = const Color(0xFF332d2b),
+      required this.text,
+      this.height = 1.2,
+      this.size = 12})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(color: color, fontFamily: 'Rboto', fontSize: size),
+    );
+  }
+}
